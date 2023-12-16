5339bd166053f1cdc44e2bac1edb044fdc4d49dd
austin_m <1austinm@protonmail.com>
2023-12-16 17:02:12 -0600

    problem 4.... too slow

---
+++ id_4/euler4.py
@@ -0,0 +1,40 @@
+
+import math
+
+def isPalindrome(number):
+    numberReversed = str(number)[::-1]
+    
+    if number == int(numberReversed):
+        return True
+    else:
+        return False
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    
+    cap = math.ceil(math.sqrt(n))
+    
+    if cap < 10:
+        # N is not large enough for 2 three digit factors
+        print("N is too small")
+        continue
+    
+    products = set()
+    factorOne = 1
+    factorTwo = 1
+    for factorOne in range(100, 1000):
+        for factorTwo in range(100, 1000):
+            p = factorOne * factorTwo
+            if p > n:
+                break
+            products.add(p)
+    
+    while products:
+        largest = max(products)
+        
+        if isPalindrome(largest):
+            print(largest)
+            break
+        else:
+            products.remove(largest)
