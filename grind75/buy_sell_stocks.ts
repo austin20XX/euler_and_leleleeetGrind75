96ee314fb8baf97d58b9790d375b99a446e1c2f6
austin_m <1austinm@protonmail.com>
2024-10-25 23:08:34 -0500

    leet 121: updated solution in relation to comment

--- grind75/buy_sell_stocks.ts
+++ grind75/buy_sell_stocks.ts
@@ -14,14 +14,16 @@ function maxProfit(prices: number[]): number {
           difference: prices[1] - prices[0],
       };
 
+  let left = 0, right;
   // The length at this point is confirmed to be at least 3
-  for(let i = 1; i < prices.length; i++) {
-      const { buyDay, sellDay, difference } = idealTransaction
-      if(prices[buyDay] > prices[i]) {
-          idealTransaction.buyDay = i;
-      } else if(prices[i] - prices[buyDay] > difference){
-          idealTransaction.sellDay = i;
-          idealTransaction.difference = prices[i] - prices[buyDay];
+  for(right = 1; right < prices.length; right++) {
+      const { difference } = idealTransaction
+      if(prices[left] > prices[right]) {
+          left = right;
+      } else if(prices[right] - prices[left] > difference){
+          idealTransaction.sellDay = right;
+          idealTransaction.buyDay = left;
+          idealTransaction.difference = prices[right] - prices[left];
       }
   }
 
@@ -30,13 +32,13 @@ function maxProfit(prices: number[]): number {
 };
 
 /** Notes:
- * In my solution, I am using the buyDay as the left pointer for the 'window'(not a real window). i is the right ptr
+ * In my previous solution, I was using the buyDay as the left pointer for the 'window'(not a real window). i(now right) was the right ptr
  * This is problematic because if a new lowest value occurs later in the array, the buyDay is updated
  * But it is not guaranteed that a greater diffence will be found after said value.
  * For example, in the test case [7, 2, 6, 7, 9, 1, 6]
  * The greatest diff is between indexes 1 & 4 with 7
  * But the buyDay will end up being index 5, since 1 is less than 2. 
- * There is no greater profit to be made after that though
+ * There is no greater profit to be made after that though, so the object becomes incorrect
  * 
  * Solution: Use seperate left pointer and only update buyDay max diff is found
  */
